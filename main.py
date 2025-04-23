abd2a8b73a5cfa7d286eb37e2e7114088659461f
Mubashir Ahmed <75428287+mubashir538@users.noreply.github.com>
2025-04-23 22:24:45 +0500

    updated the UI
    Made the UI Better for our Application

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 
 counter= 0
 
+print('---Welcome to the Counter Application---')
+print('You can stop the counter by typing "stop"')
+print('Press Enter to Increment Counter')
+print()
 while True:
     if input() == 'stop':
         break
-    counter += 1
+    counter += 2
     print(counter)
 
+print('Thank you for Using Our Counter Application')
 
